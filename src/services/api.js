0abc95cec1ffeb8ddd93fc747df01f2e78abf999
Akashkumar2010 <akashkumarar2003@gmail.com>
2025-01-15 20:57:20 +0530

    fix  isue

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL + '/endpoint';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Utility function to handle errors
 const handleError = (error, message) => {
